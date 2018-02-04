bddaed4c09d9b5e9c998421965a2fef9384c893c
zhaodifont <206033796@qq.com>
2018-02-04 14:20:21 +0800

    24r1429

--- src/static/js/bridge_android.js
+++ src/static/js/bridge_android.js
@@ -3,60 +3,78 @@ B612Kaji.Native = B612Kaji.Native || {};
 B612Kaji.Native.android = {};
 
 B612Kaji.Native.android.Function = function() {
-	this.callback = {};
+  this.callback = {};
 }
 
 B612Kaji.Native.android.Function.prototype = {
 
-	saveImage: function(callback,urlOrBase64Image) {
+	saveImage: function(url) {
 		this.callback.saveImage = function(result) {
 			var resultJson = JSON.parse(result);
-			callback(resultJson);
+			// console.log("result parsed sucess :" + resultJson.success + "result parsed errorMessage :" + resultJson.errorMessage);
 		};
-		window.B612KajiBridgeInterface.saveImage("B612Kaji.Native.android.Function.getInstance().callback.saveImage", urlOrBase64Image);
-	},
-	closeWindow: function() {
-		window.B612KajiBridgeInterface.close();
+		window.B612KajiBridgeInterface.saveImage("B612Kaji.Native.android.Function.getInstance().callback.saveImage", url);
 	},
 
-	shareImageWithCallback: function(openCallback,clickCallback,base64Image) {
-    this.callback.shareImageWithCallback = function(result) {
-      var resultJson = JSON.parse(result);
-      openCallback(resultJson);
-    };
+	shareImage: function(url) {
+		this.callback.shareImage = function(result) {
+			var resultJson = JSON.parse(result);
+			// console.log("result parsed sucess :" + resultJson.success + "result parsed errorMessage :" + resultJson.errorMessage);
+		};
+		window.B612KajiBridgeInterface.shareImage("B612Kaji.Native.android.Function.getInstance().callback.shareImage", url);
+	},
 
-    this.callback.clickShareButton = function(result) {
-      var resultJson = JSON.parse(result);
-      clickCallback(resultJson);
-    };
-
+	shareImageWithCallback: function(url, callback) {
+		this.callback.clickShareButton = function(result) {
+			var resultJson = JSON.parse(result);
+			// console.log("result parsed sucess :" + resultJson.success + "result parsed errorMessage :" + resultJson.errorMessage);
+			callback(result);
+		};
 		window.B612KajiBridgeInterface.shareImageWithCallback(
-		"B612Kaji.Native.android.Function.getInstance().callback.shareImageWithCallback",
-		"B612Kaji.Native.android.Function.getInstance().callback.clickShareButton",
-		base64Image);
+			"B612Kaji.Native.android.Function.getInstance().callback.shareImage",
+			"B612Kaji.Native.android.Function.getInstance().callback.clickShareButton",
+			url
+		);
 	},
 
-	eventCamera: function(callback,options) {
+	eventCamera: function(callback, type, cameraPosition, filterId) {
 		this.callback.eventCamera = function(result) {
 			callback(result);
 		};
-
-		window.B612KajiBridgeInterface.eventCamera("B612Kaji.Native.android.Function.getInstance().callback.eventCamera",JSON.stringify(options));
+		var extraParam = {
+			type: type
+		};
+		if (cameraPosition != undefined) {
+			extraParam.cameraPosition = cameraPosition;
+		}
+		if (filterId != undefined) {
+			extraParam.filterId = filterId;
+		}
+		window.B612KajiBridgeInterface.eventCamera("B612Kaji.Native.android.Function.getInstance().callback.eventCamera", JSON.stringify(extraParam));
 	},
 
 	getCameraImage: function(callback) {
 		this.callback.getCameraImage = function(result) {
-			result = result.replace(/\n/gi, "");
+			alert('get img');
 			callback(result);
 		};
 		window.B612KajiBridgeInterface.getCameraImage("B612Kaji.Native.android.Function.getInstance().callback.getCameraImage");
 	},
+
+	appInfo: function(callback) {
+		this.callback.appInfo = function(result) {
+			callback(result);
+		};
+		if (window.B612KajiBridgeInterface != undefined) {
+			window.B612KajiBridgeInterface.appInfo("B612Kaji.Native.android.Function.getInstance().callback.appInfo");
+		}
+	}
 }
 
 B612Kaji.Native.android.Function.getInstance = function() {
-	if (B612Kaji.Native.android.Function.instance == null) {
-		B612Kaji.Native.android.Function.instance = new B612Kaji.Native.android.Function();
-	}
-	return B612Kaji.Native.android.Function.instance;
+  if (B612Kaji.Native.android.Function.instance == null) {
+    B612Kaji.Native.android.Function.instance = new B612Kaji.Native.android.Function();
+  }
+  return B612Kaji.Native.android.Function.instance;
 }
 B612Kaji.Native.android.Function.instance = null;
